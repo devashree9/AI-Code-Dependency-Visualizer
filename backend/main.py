967518891efd8216a03be366622edd013f24e598
Devashree <rahatedevashree@gmail.com>
2026-06-15 23:48:31 +0530

    made somme changes

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://ai-code-dependency-visualizer.onrender.com/analyze"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
